2301399a28becda828d435cd0b0bee81c4a56636
Jaime Andres Atuesta <149810734+JaimeAndresAtuestaLandinez@users.noreply.github.com>
2026-05-08 14:10:18 -0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'mysql.railway.internal',
+    baseURL: import.meta.env.VITE_API_URL || '${{ MySQL.MYSQL_URL }}',
 });
 
 api.interceptors.request.use((config) => {
